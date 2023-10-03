35a3d15558f2d13e1eaaaa149b73af901b79abca
rakesh-i <rakesh.007.ac@gmail.com>
2023-10-03 19:32:39 +0530

    hour and weekday frequency

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,9 +1,13 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
+require 'date'
 
 contents = CSV.open('event_attendees.csv', headers:true,
 header_converters: :symbol)
+data = contents.read
+size =  data.length
+contents.rewind
 
 puts 'EventManager initialized.'
 
@@ -15,6 +19,18 @@ def save_thank_you_letter(id, form_letter)
   File.open(file_name, 'w') do |file|
     file.puts form_letter
   end
+end
+
+def valid_phone_number(phone)
+    phone = phone.to_s.scan(/\d+/).join("")
+    if phone.length==10
+        return phone
+    elsif phone.length==11 && phone[0]=="1"
+        phone = phone.slice(1,11)
+        return phone
+    else
+        return "Invalid"
+    end
 end
 
 def clean_zipcode(zipcode)
@@ -46,14 +62,36 @@ template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
 
+# contents.each do |row|
+#     id = row[0]
+#     name = row[:first_name]
+#     zipcode = clean_zipcode(row[:zipcode])
+#     legislators = legislators_by_zipcode(zipcode)
+
+#     form_letter = erb_template.result(binding)
+
+#     save_thank_you_letter(id, form_letter)
+
+# end
+
+# contents.each do |row|
+#     puts valid_phone_number(row[:homephone])
+# end
+array = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+hour = Hash.new(0)
+day = Hash.new(0)
 contents.each do |row|
-    id = row[0]
-    name = row[:first_name]
-    zipcode = clean_zipcode(row[:zipcode])
-    legislators = legislators_by_zipcode(zipcode)
+#   time = row[:regdate].to_s.split(" ")[1].split(":")[0]
+    date = row[:regdate]
+    time = DateTime.strptime(date, "%m/%d/%y %H:%M")
+    hour[time.hour] += 1
+    day[time.wday] +=1
+    # puts time.hour
+end
+hour.each do |key, value|
+    puts "#{key} #{value}"
+end
 
-    form_letter = erb_template.result(binding)
-
-    save_thank_you_letter(id, form_letter)
-
+day.each do|key, value|
+  puts "#{array[key]} #{value}"
 end
